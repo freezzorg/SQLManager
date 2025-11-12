8fd1d59225d71de754becd70247af728b6f97b56
FreezZorg <freezz@pm.me>
2025-11-12 14:10:31 +0500

    FIX Подправил формат строка доступных бат окончания бэкапа.

--- static/script.js
+++ static/script.js
@@ -575,8 +575,34 @@ document.addEventListener('DOMContentLoaded', () => {
                         parseInt(timePart[1]), 
                         parseInt(timePart[2])
                     );
+                    
+                    // Форматируем дату вручную в формате ДД.ММ.ГГГ ЧЧ:мм:сс
+                    const day = String(date.getDate()).padStart(2, '0');
+                    const month = String(date.getMonth() + 1).padStart(2, '0');
+                    const year = date.getFullYear();
+                    const hours = String(date.getHours()).padStart(2, '0');
+                    const minutes = String(date.getMinutes()).padStart(2, '0');
+                    const seconds = String(date.getSeconds()).padStart(2, '0');
+                    
+                    // Определяем тип бэкапа для отображения
+                    let backupType;
+                    switch(item.Type) {
+                        case "Database":
+                            backupType = "Полный бэкап";
+                            break;
+                        case "Database Differential":
+                            backupType = "Дифференциальный бэкап";
+                            break;
+                        case "Transaction Log":
+                            backupType = "Бэкап журналов транзакций";
+                            break;
+                        default:
+                            backupType = item.Type;
+                    }
+                    
                     option.value = formatDateTime(date, 'input');
-                    option.textContent = date.toLocaleString('ru-RU');
+                    option.textContent = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}  - ${backupType}`;
+                    // option.textContent = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}  - ${item.Type}`;
                     backupEndTimesSelect.appendChild(option);
                 });
                 
